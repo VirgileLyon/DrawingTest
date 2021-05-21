b49379f150270142a4dfcc3c09c2e08f55ec26e1
VirgileLyon <45287046+VirgileLyon@users.noreply.github.com>
2021-05-21 15:29:05 +0200

    Add files via upload

---
+++ DrawingTest/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Virgile Vercellis on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
